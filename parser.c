dd6e682025882c8ebae47d5794fb52a3e1464ec0
py-cc <paolacarrero731@gmail.com>
2021-04-03 16:42:28 -0500

    add file

--- parser.c
+++ parser.c
@@ -1,10 +1,20 @@
 #include "shell.h"
 
+#define DELIMITADOR " "
 
-char *parser(char *string)
+
+list_t *parser(char *string)
 {
+	int i;
+	char *token;
+	list_t *tokens;
 
+	tokens = new_list(strtok(string, " "));
 
+	for (i = 0; ;i++)
+	{
+		add_list(tokens, strtok(NULL, " "));
+	}
 
+	return(*tokens)
 }
-
