30d802336a2e651d67de7441409407d64a4412e7
JuanDAC <juanavila12.0308@gmail.com>
2021-04-06 21:51:59 +0000

    [docs ~ prompt.c](delete '\n' inlast position of a line read)

--- prompt.c
+++ prompt.c
@@ -12,6 +12,7 @@ char *prompt(void)
 
 	write(1, SIGN, 2);
 	length_characters = getline(&buffer, &length, stdin);
+	/*delete '\n' in last character */
 	buffer[length_characters - 1] = '\0';
 	write(1, buffer, length_characters);
 	return (buffer);
