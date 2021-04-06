78e116ac92b5b8ec45e92130654a52f63d2a32d2
py-cc <paolacarrero731@gmail.com>
2021-04-06 16:59:54 -0500

    add file to check

---
+++ parser_basic.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+
+#define DELIMITADOR (" ")
+
+void **parser(char *string) {
+	int i, status;
+	char *current_string;
+	char *current_token = NULL;
+	char **args;
+	pid_t pid;
+
+
+	current_token = strtok(string, " ");
+	args = malloc(sizeof(char *) 10);
+
+	i = 0;
+	while(current_token != NULL)
+	{
+		args[i] = strdup(token);
+		current_token = strtok(NULL, " ");
+		i++;
+	}
+	args[i] = token;
+
+	pid = fork();
+
+	if (!pid)
+	{
+			i = 0;
+			while(args[i] != NULL)
+			{
+					if (execve(args[i], args, NULL) == -1)
+					{
+							perror("Error:");
+					}
+					i++;
+			}	
+	}
+	else
+	{
+			wait(&status);
+	} 
+
+}
+
