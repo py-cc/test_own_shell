57ac9512e59e8283905e85ba1358cf55717bcc87
py-cc <paolacarrero731@gmail.com>
2021-04-06 20:10:26 -0500

    add file

--- parser_basic.c
+++ parser_basic.c
@@ -2,17 +2,61 @@
 
 #define DELIMITADOR (" ")
 
-bool equal_strings(char *string, char *other_string)
+/**
+* lenght - Entry point
+* @string: int
+* Return: Always 0 (Success)
+*/
+int lenght(char *string)
 {
-	if (!string || !other_string)
+	return ((*string == '\0') ? 0 : 1 + lenght(string + 1));
+}
+/**
+* str_concat - Entry point
+* @s1: char
+* @s2: char
+* Return: Always 0 (Success)
+*/
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, j, length_s1 = 0, length_s2 = 0;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = ("");
+
+	if (s2 == NULL)
+		s2 = ("");
+
+	length_s1 = lenght(s1);
+	length_s2 = lenght(s2);
+	str = malloc(sizeof(char) * (length_s1 + length_s2 - 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		*(str + i) = *(s1 + i);
+
+	for (j = 0; *(s2 + j) != '\0'; i++, j++)
+		*(str + i) = *(s2 + j);
+
+	*(str + i) = *(s2 + j);
+	return (str);
+}
+
+bool equal_strings(char *string, char *search_string, const bool variadic, ...)
+{
+	(void)variadic;
+	if (!string || !search_string)
 		return (false);
-	if (!*string && !*other_string)
+	if (!*string && !*search_string)
 		return (true);
 
-	if (*string != *other_string)
+	if (*string != *search_string)
 		return (false);
 
-	return (equal_strings(string + 1, other_string + 1));
+	return (equal_strings(string + 1, search_string + 1, variadic));
 }
 
 void **parser(char *string) {
@@ -23,6 +67,8 @@ void **parser(char *string) {
 	pid_t pid;
 	extern char **environ;
 	char *path_content = "hola como estas";
+	char *current_direction;
+
 
 	current_token = strtok(string, " ");
 	args = malloc(sizeof(char *) * 10);
@@ -40,37 +86,32 @@ void **parser(char *string) {
 	/* leer el PATH y traer las direcciones y concatenarlas con el comando que nos llegue*/
 	for (i = 0; environ[i]; i++)
 	{
-		if (strtok(environ[i], "=") == "PATH")
-			while (path_content)
+		if (equal_strings(strtok(environ[i], "="), "PATH", false))
+			while (path_content) /* <<<< do while, pendiente*/
 			{
 				path_content = strtok(NULL, ":");
-				args[0] /* ls */
-				args[0] = strconcat("/", args[0]);
-				args[0] = strconcat(path_content, args[0]);
-					/* /usr/local/sbin/ls */
+				current_string = str_concat("/", args[0]); /* <<<< /ls*/
+				current_direction = str_concat(path_content, current_string); /* <<<< /bin/ls*/
+				if (access(current_direction, X_OK) == 0)
+						break;
 			}
 	}
-	
+	args[0] = current_direction;
 
 	/* chear el hijo */
 	pid = fork();
 
 	if (!pid)
 	{
-			i = 0;
-			while(args[i] != NULL)
+			if (execve(args[0], args, NULL) == -1)
 			{
-					if (execve(args[i], args, NULL) == -1)
-					{
-							perror("Error:");
-					}
-					i++;
+				perror("Error:");
 			}	
 	}
 	else
 	{
 			wait(&status);
-	} 
-
+	}
+	return ((void **)args);
 }
 
